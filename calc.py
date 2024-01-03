b1815acadc5fa2c8e3918747f846271cf2352e82
SuperFeda <112332753+SuperFeda@users.noreply.github.com>
2024-01-03 17:58:54 +0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
 while True:
-    try: print(f'{eval(input("Введите выражение ---> ")):,}')
-    except ZeroDivisionError as e: print(f"На ноль делить нельзя! {e}")
-    except NameError as e: print(f"Выражение введено не верно. {e}")
+    try: print(f'{eval(input("Введите выражение ---> ")):,}'); break
+    except ZeroDivisionError as e: print(f"На ноль делить нельзя! {e}"); continue
+    except NameError as e: print(f"Выражение введено не верно. {e}"); continue
